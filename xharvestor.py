1eb3bcc48b482b03e409f050c949c095e4d0bb64
n3tworkh4x <networkmandaean@gmail.com>
2025-08-19 14:09:42 +0000

    some banners

--- xharvestor.py
+++ xharvestor.py
@@ -1,1 +1,61 @@
 #!/bin/env python3
+
+import subprocess
+import argparse
+import sys
+import time
+import csv
+from typing import List, Dict, Any
+
+# Color setup
+try:
+    import colorama
+    colorama.init(autoreset=True)
+    GREEN = colorama.Fore.GREEN
+    YELLOW = colorama.Fore.YELLOW
+    RED = colorama.Fore.RED
+    BLUE = colorama.Fore.BLUE
+    MAGENTA = colorama.Fore.MAGENTA
+    CYAN = colorama.Fore.CYAN
+    RESET = colorama.Fore.RESET
+except ImportError:
+    # Fallback if colorama is not installed
+    GREEN = YELLOW = RED = BLUE = MAGENTA = CYAN = RESET = ""
+
+def banner() -> None:
+    """Banner for the project"""
+    print(f"""{MAGENTA}
+ _  _  _   _    __    ____  _  _  ____  ___  ____  _____  ____ 
+( \/ )( )_( )  /__\  (  _ \( \/ )( ___)/ __)(_  _)(  _  )(  _ \\
+ )  (  ) _ (  /(__)\  )   / \  /  )__) \__ \  )(   )(_)(  )   /
+(_/\_)(_) (_)(__)(__)(_)\_)  \/  (____)(___/ (__) (_____)(_)\_)
+    {RESET}""")
+
+def print_status(message: str) -> None:
+    """Print status messages"""
+    print(f"{GREEN}[+]{RESET} {message}")
+
+def print_warning(message: str) -> None:
+    """Print warning messages"""
+    print(f"{YELLOW}[!]{RESET} {message}")
+
+def print_error(message: str) -> None:
+    """Print error messages"""
+    print(f"{RED}[-]{RESET} {message}")
+
+
+class WiFiModule:
+    """WiFi reconnaissance module"""
+    
+    def __init__(self, interface: str = "wlo1"):
+        self.interface = interface
+        self.monitor_interface = f"{interface}mon"
+        self.scan_results = []
+    
+    def setup_monitor_mode(self) -> bool:
+        """Put wireless interface into monitor mode"""
+        print_status(f"Setting up monitor mode on {self.interface}")              
+
+if __name__ == '__main__':
+    wifi = WiFiModule()
+    banner()
