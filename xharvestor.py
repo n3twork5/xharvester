8b3f4be9495cba4d8d9494fc4148c28e9b4c40d5
n3tworkh4x <networkmandaean@gmail.com>
2025-08-19 15:42:40 +0000

    Update xharvestor.py

--- xharvestor.py
+++ xharvestor.py
@@ -1,5 +1,5 @@
 #!/bin/env python3
-
+import shlex
 import subprocess
 import argparse
 import sys
@@ -23,14 +23,14 @@ except ImportError:
     GREEN = YELLOW = RED = BLUE = MAGENTA = CYAN = RESET = ""
 
 def banner() -> None:
-    """Banner for the project"""
+    """Banner"""
     print(f"""{MAGENTA}
  _  _  _   _    __    ____  _  _  ____  ___  ____  _____  ____ 
 ( \/ )( )_( )  /__\  (  _ \( \/ )( ___)/ __)(_  _)(  _  )(  _ \\
  )  (  ) _ (  /(__)\  )   / \  /  )__) \__ \  )(   )(_)(  )   /
 (_/\_)(_) (_)(__)(__)(_)\_)  \/  (____)(___/ (__) (_____)(_)\_)
     {RESET}""")
-
+    print(f"{CYAN}>>> Extented Reconnaissance Toolkit For Pentesters <<<{RESET}")
 def print_status(message: str) -> None:
     """Print status messages"""
     print(f"{GREEN}[+]{RESET} {message}")
@@ -47,15 +47,33 @@ def print_error(message: str) -> None:
 class WiFiModule:
     """WiFi reconnaissance module"""
     
-    def __init__(self, interface: str = "wlo1"):
+    def __init__(self, interface: str) -> None:
         self.interface = interface
         self.monitor_interface = f"{interface}mon"
         self.scan_results = []
     
-    def setup_monitor_mode(self) -> bool:
+    def setup_monitor_mode(self) -> str:
         """Put wireless interface into monitor mode"""
-        print_status(f"Setting up monitor mode on {self.interface}")              
+        print_status(f"{YELLOW}Setting up monitor mode on {self.interface}{RESET}") 
+        cmd = f"sudo ifconfig {self.interface} down && sudo iwconfig {self.interface} mode monitor && sudo ifconfig {self.interface} up && sudo airmon-ng check kill"
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        if output:
+            print(f'{GREEN}[+] Successfully set to monitor mode: {self.interface}!{RESET}')
+        else:
+            print('An error occured during change of mode!')
+    
+    def setup_manage_mode(self) -> str:
+        print_status(f"{YELLOW}Setting sup manage mode on {self.interface}{RESET}")
+        cmd = f"sudo ifconfig {self.interface} down && sudo iwconfig {self.interface} mode manage && sudo ifconfig {self.interface} up && sudo service NetworkManager restart"
+        output = subprocess.check_output(cmd, shell=True, text=True)
+        if output:
+            print_status(f'{YELLOW}[+] Successfully set to manage mode: {self.interface}!{RESET}')
+        else:
+            print_error('An error occured during set of mode!')
+    
 
 if __name__ == '__main__':
-    wifi = WiFiModule()
+    wifi = WiFiModule('wlo1')
+    wifi.setup_monitor_mode()
     banner()
+    wifi.setup_manage_mode()
